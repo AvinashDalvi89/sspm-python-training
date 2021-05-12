1ce9556ea5a909daea4ce4d18f5eba06ceb6aec1
aviboy2006 <avianshsdalvi@gmail.com>
2021-05-12 08:38:09 +0530

    Added add to cart functionality

--- ecommerce-api/app.py
+++ ecommerce-api/app.py
@@ -23,6 +23,12 @@ def after_request(response):
 def hello():
     return "Welcome to Flask World!😃"
 
+#this is to throw error which method is allow
+@app.errorhandler(405)
+def not_found(error):
+    return make_response(jsonify({'error': 'Method Not found','status': 405}), 405)
+
+
 @app.route("/get-products")
 def get_products():
     sql = "select * from tbl_products "
@@ -118,3 +124,53 @@ def update_product(id):
         return {"status": "Successfully updated product"}
     else:
         return {"status": "Error in updating product"}
+
+@app.route('/delete/product/<id>', methods=['POST'])
+def delete_product(id):
+    
+    #print(today_date_time)
+    update_sql = "DELETE FROM tbl_products WHERE id = " + str(id)+""
+
+    my_cursor.execute(update_sql)
+
+    my_db.commit()
+    if my_cursor.rowcount > 0:
+        return {"status": "Successfully deleted product"}
+    else:
+        return {"status": "Error in deleting product"}
+
+
+@app.route('/add/cart', methods=['POST'])
+def add_to_cart():
+    params = request.get_json()
+    product_id = params.get('product_id')
+    quantity = params.get('quantity')
+    get_product_sql = "select * from tbl_products where id = " + str(product_id) + ""
+    my_cursor.execute(get_product_sql)
+    product_result = my_cursor.fetchone()
+    column_list = ["id", "name", "category", "subCategory", "availableQuantity", "description",
+                   "price", "brand"]
+    product_dict = {}
+    i = 0
+    for product in product_result:
+        if i < len(column_list):
+            product_dict[column_list[i]] = product
+            # print(product_dict)
+            i = i + 1
+
+    cart_price = quantity * product_dict['price']
+    shipping_charges = (1 * cart_price)/100
+    total_cart_price = cart_price + shipping_charges
+    today_date_time = datetime.datetime.now()
+    today_date_time = today_date_time.strftime("%Y-%m-%d %H:%M:%S")
+    insert_sql = "INSERT INTO tbl_carts(product_id,quantity, cart_value,shipping_charges,total_cart_value,created_at,modified_at,created_by," \
+                 "modified_by) VALUES (%s, %s,%s, %s,%s, %s,%s,1,1)"
+
+    insert_val = (product_id,quantity,cart_price,shipping_charges,total_cart_price,today_date_time,today_date_time)
+    my_cursor.execute(insert_sql, insert_val)
+    my_db.commit()
+    if my_cursor.rowcount > 0:
+        return {"status": "Successfully added product to cart"}
+    else:
+        return {"status": "Error in adding product to cart"}
+
